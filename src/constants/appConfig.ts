92df07096dc7a9c9bd0f6a1a22456f00c109defc
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-14 23:33:09 +0100

    refactor: change src/utils/AppConfig.ts to src/constants/appConfig.ts

---
+++ src/constants/appConfig.ts
@@ -0,0 +1,9 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Wallet Explorer',
+  title: 'Wallet Explorer',
+  description:
+    'a read-only wallet explorer that allows users to input an address and retrieve token balances for that address across multiple EVM chains',
+  locale: 'en',
+};
