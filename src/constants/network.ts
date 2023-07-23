e657c9a0c0ce45f9d71e1b75b07ea2cbdcd21724
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-23 10:30:38 +0100

    feat: use SupportedChainId

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,13 +1,24 @@
+export enum SupportedChainId {
+  MAINNET = 1,
+
+  ARBITRUM_ONE = 42161,
+
+  OPTIMISM = 10,
+
+  BSC = 56,
+
+  POLYGON = 137,
+}
+
 export const RPC_URLS = {
-  1: [
+  [SupportedChainId.MAINNET]: [
     // "Safe" URLs
-    'https://api.mycryptoapi.com/eth',
     'https://cloudflare-eth.com',
     // "Fallback" URLs
     'https://rpc.ankr.com/eth',
     'https://eth-mainnet.public.blastapi.io',
   ],
-  137: [
+  [SupportedChainId.POLYGON]: [
     // "Safe" URLs
     'https://polygon-rpc.com/',
     'https://rpc-mainnet.matic.network',
@@ -16,19 +27,19 @@ export const RPC_URLS = {
     'https://rpc-mainnet.matic.quiknode.pro',
     'https://matic-mainnet-full-rpc.bwarelabs.com',
   ],
-  42161: [
+  [SupportedChainId.ARBITRUM_ONE]: [
     // "Safe" URLs
     'https://arb1.arbitrum.io/rpc',
     // "Fallback" URLs
     'https://arbitrum.public-rpc.com',
   ],
-  10: [
+  [SupportedChainId.OPTIMISM]: [
     // "Safe" URLs
     'https://mainnet.optimism.io/',
     // "Fallback" URLs
     'https://rpc.ankr.com/optimism',
   ],
-  56: [
+  [SupportedChainId.BSC]: [
     // "Safe" URLs
     'https://endpoints.omniatech.io/v1/bsc/mainnet/public',
     'https://bsc-mainnet.gateway.pokt.network/v1/lb/6136201a7bad1500343e248d',
@@ -40,18 +51,14 @@ export const RPC_URLS = {
     'https://bsc-dataseed4.defibit.io',
     'https://rpc.ankr.com/bsc',
   ],
-  43114: [
-    // "Safe" URLs
-    'https://api.avax.network/ext/bc/C/rpc',
-    'https://avalanche-c-chain.publicnode.com',
-  ],
 };
 
+export type SupportedChainsType = keyof typeof RPC_URLS;
+
 export const CHAIN_IDS_TO_NAMES = {
-  1: 'mainnet',
-  137: 'polygon',
-  42161: 'arbitrum',
-  10: 'optimism',
-  56: 'bnb',
-  43114: 'avalanche',
+  [SupportedChainId.MAINNET]: 'mainnet',
+  [SupportedChainId.POLYGON]: 'polygon',
+  [SupportedChainId.ARBITRUM_ONE]: 'arbitrum',
+  [SupportedChainId.OPTIMISM]: 'optimism',
+  [SupportedChainId.BSC]: 'bsc',
 } as const;
