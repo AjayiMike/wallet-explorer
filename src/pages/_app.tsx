52474f18dfb2df9ae3f0d3ded0a4c60ca291d681
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-23 10:38:05 +0100

    feat: configure redux-multicall

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,17 @@ import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
 
+import { BlockNumberProvider } from '@/hooks/useBlockNumber';
+import MulticallUpdater from '@/multicall';
+
 import { store } from '../state';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ReduxProvider store={store}>
-    <Component {...pageProps} />
+    <BlockNumberProvider>
+      <MulticallUpdater />
+      <Component {...pageProps} />
+    </BlockNumberProvider>
   </ReduxProvider>
 );
 
