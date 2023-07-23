1557d993a0526cb69ff889a38aaaa13fddbbb32a
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-23 10:35:00 +0100

    feat: add application state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { multicall } from '@/multicall';
+
+import application from './application/reducer';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    application,
+    multicall: multicall.reducer,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export default store;
+
+export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
