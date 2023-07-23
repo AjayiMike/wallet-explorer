28b7cd4bddd50d346e4347559e921840c637f2fa
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-23 22:08:47 +0100

    feat: update app state by adding lists reducer

--- src/state/index.ts
+++ src/state/index.ts
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { multicall } from '@/multicall';
 
 import application from './application/reducer';
+import lists from './lists/reducer';
 
 export const store = configureStore({
   reducer: {
     application,
+    lists,
     multicall: multicall.reducer,
   },
 });
