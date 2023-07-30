ab07d662bde56c3ad16aa901d9655c1acedb3882
AjayiMike <ajayimikeofficial@gmail.com>
2023-07-31 00:31:57 +0100

    feat: render tokens with balance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,59 @@
+import type { Currency, Token } from '@uniswap/sdk-core';
+import type { CSSProperties } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
+import { FixedSizeList } from 'react-window';
+
 import ChainDropdown from '@/components/chainsDropdown';
-import TokenComponent from '@/components/tokenComponent';
+import TokenLogo from '@/components/tokenLogo';
 import WalletInput from '@/components/walletInput';
 import { AppConfig } from '@/constants/appConfig';
+import { useAllTokens } from '@/hooks/tokens';
+import useCurrencyBalance, {
+  useAllTokenBalances,
+} from '@/hooks/useCurrencyBalance';
 import { Main } from '@/layouts/Main';
 import { Meta } from '@/layouts/Meta';
+import { useAppSelector } from '@/state/hooks';
+import { currencyKey } from '@/utils';
+import { tokenComparator } from '@/utils/sorting';
 
 const Index = () => {
+  // refs for fixed size lists
+  const fixedList = useRef<FixedSizeList>();
+  const allTokens = useAllTokens();
+  const tokensArray = Object.values(allTokens);
+  const [balances, balancesAreLoading] = useAllTokenBalances();
+
+  // console.log({ balances, balancesAreLoading });
+
+  // console.log({ allTokens });
+
+  const sortedTokens: Token[] = useMemo(
+    () =>
+      !balancesAreLoading
+        ? tokensArray.sort(tokenComparator.bind(null, balances))
+        : [],
+    [balances, balancesAreLoading],
+  );
+
+  console.log('sortedTokens: ', sortedTokens);
+
+  const itemKey = useCallback((index: number, data: typeof sortedTokens) => {
+    const currency = data[index];
+    return currencyKey(currency!);
+  }, []);
+
+  interface TokenRowProps {
+    data: Array<Currency>;
+    index: number;
+    style: CSSProperties;
+  }
+
+  const Row = useCallback(({ data, index, style }: TokenRowProps) => {
+    const token: Currency = data[index]!;
+    return <CurrencyRow currency={token} style={style} />;
+  }, []);
+
   return (
     <Main
       meta={
@@ -18,13 +66,45 @@ const Index = () => {
           <ChainDropdown />
         </div>
         <div className="">
-          {new Array(10).fill(undefined).map((_, index) => (
-            <TokenComponent key={index} />
-          ))}
+          <FixedSizeList
+            ref={fixedList as any}
+            width="100%"
+            height={1200}
+            itemData={sortedTokens}
+            itemCount={sortedTokens.length}
+            itemSize={70}
+            itemKey={itemKey}
+          >
+            {Row}
+          </FixedSizeList>
         </div>
       </div>
     </Main>
   );
 };
+
+export function CurrencyRow({
+  currency,
+  style,
+}: {
+  currency: Currency;
+  style?: CSSProperties;
+}) {
+  const account = useAppSelector((state) => state.application.address);
+  const balance = useCurrencyBalance(account ?? undefined, currency);
+
+  return (
+    <div style={style} className="flex items-center gap-6 text-base">
+      <TokenLogo token={currency} />
+      <div className="flex flex-col">
+        <span className="font-bold">{currency.symbol}</span>
+        <span className="text-sm">{currency.name}</span>
+      </div>
+      <div className="flex-1 text-right font-semibold">
+        {balance?.toFixed(3)}
+      </div>
+    </div>
+  );
+}
 
 export default Index;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,69 @@
+import type { Currency } from '@uniswap/sdk-core';
+import { getAddress } from 'ethers';
+import type { TokenInfo, TokenList } from 'tokenList';
+
+import { WrappedTokenInfo } from '@/state/lists/wrappedTokenInfo';
+
 export function generateId() {
   const timestamp = Date.now().toString(36);
   const randomStr = Math.random().toString(36).substr(2, 5);
 
   return `${timestamp}-${randomStr}`;
+}
+
+// returns the checksummed address if the address is valid, otherwise returns false
+export function isAddress(value: any): string | false {
+  try {
+    // Alphabetical letters must be made lowercase for getAddress to work.
+    // See documentation here: https://docs.ethers.io/v5/api/utils/address/
+    return getAddress(value.toLowerCase());
+  } catch {
+    return false;
+  }
+}
+
+type TokenMap = Readonly<{
+  [tokenAddress: string]: { token: WrappedTokenInfo; list?: TokenList };
+}>;
+export type ChainTokenMap = Readonly<{ [chainId: number]: TokenMap }>;
+
+type Mutable<T> = {
+  -readonly [P in keyof T]: Mutable<T[P]>;
+};
+const mapCache =
+  typeof WeakMap !== 'undefined'
+    ? new WeakMap<TokenList | TokenInfo[], ChainTokenMap>()
+    : null;
+
+export function tokensToChainTokenMap(
+  tokens: TokenList | TokenInfo[],
+): ChainTokenMap {
+  const cached = mapCache?.get(tokens);
+  if (cached) return cached;
+
+  const [list, infos] = Array.isArray(tokens)
+    ? [undefined, tokens]
+    : [tokens, tokens.tokens];
+  const map = infos.reduce<Mutable<ChainTokenMap>>((map, info) => {
+    try {
+      const token = new WrappedTokenInfo(info, list);
+      if (map[token.chainId]?.[token.address] !== undefined) {
+        console.warn(`Duplicate token skipped: ${token.address}`);
+        return map;
+      }
+      if (!map[token.chainId]) {
+        map[token.chainId] = {};
+      }
+      map[token.chainId]![token.address] = { token, list };
+      return map;
+    } catch {
+      return map;
+    }
+  }, {}) as ChainTokenMap;
+  mapCache?.set(tokens, map);
+  return map;
+}
+
+export function currencyKey(currency: Currency): string {
+  return currency.isToken ? currency.address : 'ETHER';
 }
